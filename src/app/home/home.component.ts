332526437ef4a32c2d8e5e47e07ff9d9fd8045ad
Caleb Besser <cebesser@csuchico.edu>
2023-06-21 14:33:52 -0700

    Finished homepage, finished contacts page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  slideIndex = 1;
   
+
+  plusSlides(n:number){
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n:number){
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(slideIndex:number):any;
+  showSlides(n:number){
+    let i;
+    let slides = document.getElementsByClassName("slides") as HTMLCollectionOf<HTMLDivElement>;
+    let dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLSpanElement>;
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    if(slides[this.slideIndex-1])
+    slides[this.slideIndex-1].style.display = "block";
+    if(dots[this.slideIndex-1])
+    dots[this.slideIndex-1].className += " active";
+  }
 }
 
